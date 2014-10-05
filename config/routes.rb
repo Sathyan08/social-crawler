31022c024a3b68493dfce9231bd11c711dbe9385
Sathyan Mathai <Sathyan.Mathai@gmail.com>
2014-10-05 11:25:38 -0400

    Added links for sign in and sign out to layout.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-  get "/auth/:provider/callback" => "sessions#create"
-  get "/signout" => "sessions#destroy", :as => :signout
+  get "/auth/:provider/callback", to: "sessions#create"
+  get "/signout", to: "sessions#destroy", :as => :signout
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
