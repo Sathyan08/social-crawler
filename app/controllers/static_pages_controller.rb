f8fd3da7963b79281c1da09c8b365befc3a75888
Sathyan Mathai <Sathyan.Mathai@gmail.com>
2014-10-19 19:15:08 -0400

    got highlighting on hover

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,9 +2,9 @@ class StaticPagesController < ApplicationController
   def home
     if current_user
       redirect_to user_path(current_user)
+    else
+      redirect_to "/about"
     end
-
-    redirect_to "/about"
   end
 
   def about
